6e8fe23b0e0878934c56bc6bd7eb0bf8ca475849
mattbat131 <sternm@bxscience.edu>
2014-01-07 08:41:05 -0500

    Added Pancakes and Flip function

--- Pancakes.c
+++ Pancakes.c
@@ -20,7 +20,24 @@ void flip (int i, int pancakes[])
     }
 }
 
+int pancakes (int totalPan, int randNumb[])
+{
+  //Random Generator
+  srand(time(NULL));
+  for (int i = 0; i <= totalPan; i++)
+  {
+    randNumb[i] = rand() % 0;
+    return randNumb[i];
+  }
+}
+
 int main (int argc, char *argv[])
 {
+  //Gets The Random Pancake Sizes
+  int totalPan;
+  scanf("%d", &totalPan);
+  int randNumb[totalPan];
+  randNumb = pancakes(totalPan, randNumb);
+  
   return 0;
 }
